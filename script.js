922175314860426342f84bbaabd53edcf24497f0
Ricardo Javier Maldonado <javier.maldonado.sm@gmail.com>
2022-10-27 23:12:43 -0300

    se agrego menu hamburguesa y funciona

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-//  const header_btn = document.querySelector ('.hamburguesa-btn')
-//  const header_menu = document.querySelector('.main-ul')
-
-
-//  header_btn.addEventListener('click', () => {     header_menu.style.display = 'block'
-//  })
-
-// const navToggle = document.querySelector(".hamburguesa-btn")
-// const navMenu = document.querySelector(".main-ul")
-
-// navToggle.addEventListener("click", () => {
-//     navMenu.classList.toggle("nav-menu_visible");
-// });
-
-
 const navToggle = document.querySelector(".nav-toggle")
 const navMenu = document.querySelector(".nav-menu")
 
